71bdb9d8d1b5438995a36ce0cb9fa07c8c98c8a4
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2019-11-05 23:29:52 -0300

    Updates

---
+++ precache-manifest.f12dcab68c5052c31cd2dfa9d21517a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfaa7a340e4e2bdf101be2af3b0328a5",
+    "url": "/react-itunes-api/index.html"
+  },
+  {
+    "revision": "7e1a0577338c5541e781",
+    "url": "/react-itunes-api/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "240f5aa1020f00bbf154",
+    "url": "/react-itunes-api/static/js/2.4d31d063.chunk.js"
+  },
+  {
+    "revision": "7e1a0577338c5541e781",
+    "url": "/react-itunes-api/static/js/main.4475cdbe.chunk.js"
+  },
+  {
+    "revision": "089ba67d71be1d0abe4d",
+    "url": "/react-itunes-api/static/js/runtime-main.6567df6f.js"
+  }
+]);
